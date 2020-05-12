5a060e325f65c4ec625592f2fbb80ce66b704007
L.Rain <bxy2997@sina.com>
2020-05-12 17:25:39 +0800

    添加eslint

--- packages/cli/bin/fantuan.js
+++ packages/cli/bin/fantuan.js
@@ -1,25 +1,22 @@
 #!/usr/bin/env node
 
-import program from "commander";
-import inquirer from "inquirer";
-import { git } from "../lib/promptModules/index.js";
-import checkNodeVersion from "../lib/util/checkNodeVersion.js";
-import { createRequire } from "module";
-const require = createRequire(import.meta.url);
-const requiredNodeVersion = require("../package.json");
+import program from 'commander'
+import inquirer from 'inquirer'
+import { git } from '../lib/promptModules/index.js'
+import checkNodeVersion from '../lib/util/checkNodeVersion.js'
+import { createRequire } from 'module'
+const require = createRequire(import.meta.url)
+const requiredNodeVersion = require('../package.json')
 
-console.log('in');
 // 检测node版本是否符合要求
-checkNodeVersion(requiredNodeVersion.engines.node,'@fanstuan/cli')
+checkNodeVersion(requiredNodeVersion.engines.node, '@fanstuan/cli')
 
-program.command("download").action(() => {
-  inquirer
-    .prompt(git.prompt)
-    .then(async (answers) => {
-      await git.onPromptComplete({answers}).catch(e=>{
-        console.log(e);
-      })
-    });
-});
+program.command('download').action(() => {
+  inquirer.prompt(git.prompt).then(async answers => {
+    await git.onPromptComplete({ answers }).catch(e => {
+      console.log(e)
+    })
+  })
+})
 
-program.parse(process.argv);
+program.parse(process.argv)
