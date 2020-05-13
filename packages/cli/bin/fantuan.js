a8743ef825f138611d878ca7346462df26839f95
L.Rain <bxy2997@sina.com>
2020-05-13 10:40:49 +0800

    添加命令行错误智能提示工具

--- packages/cli/bin/fantuan.js
+++ packages/cli/bin/fantuan.js
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
-
+import chalk from 'chalk'
 import program from 'commander'
 import inquirer from 'inquirer'
 import { git } from '../lib/promptModules/index.js'
 import checkNodeVersion from '../lib/util/checkNodeVersion.js'
+import suggestCommands from '../lib/util/suggestCommands.js'
 import { createRequire } from 'module'
 const require = createRequire(import.meta.url)
 const requiredNodeVersion = require('../package.json')
@@ -11,12 +12,23 @@ const requiredNodeVersion = require('../package.json')
 // 检测node版本是否符合要求
 checkNodeVersion(requiredNodeVersion.engines.node, '@fanstuan/cli')
 
-program.command('download').action(() => {
-  inquirer.prompt(git.prompt).then(async answers => {
-    await git.onPromptComplete({ answers }).catch(e => {
-      console.log(e)
+program
+  .command('download')
+  .description('选择一个远程仓库下载到本地')
+  .action(() => {
+    inquirer.prompt(git.prompt).then(async answers => {
+      await git.onPromptComplete({ answers }).catch(e => {
+        console.log(e)
+      })
     })
   })
+
+// 处理非法命令
+program.arguments('<command>').action(cmd => {
+  // 不退出输出帮助信息
+  program.outputHelp()
+  console.log('  ' + chalk.red(`Unknown command ${chalk.yellow(cmd)}.`))
+  suggestCommands(cmd)
 })
 
 program.parse(process.argv)
--- packages/cli/lib/util/suggestCommands.js
+++ packages/cli/lib/util/suggestCommands.js
@@ -1,4 +1,5 @@
 import didYouMean from 'didYouMean' // 简易的智能匹配引擎
+import program from 'commander'
 import chalk from 'chalk'
 export default function suggestCommands (cmd) {
   const avaliableCommands = program.commands.map(cmd => {
@@ -7,6 +8,6 @@ export default function suggestCommands (cmd) {
   // 简易智能匹配用户命令
   const suggestion = didYouMean(cmd, avaliableCommands)
   if (suggestion) {
-    console.log(`  ` + chalk.red(`Did you mean ${chalk.yellow(suggestion)}?`))
+    console.log('  ' + chalk.red(`Did you mean ${chalk.yellow(suggestion)}?`))
   }
 }
