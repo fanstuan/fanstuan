1d6009a5ca7d95b27c4e4ee07fbc710abd35f85b
L.Rain <bxy2997@sina.com>
2020-06-15 22:03:20 +0800

    fix(rollup配置文件): 打包后文件去除了无用的包引用

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import json from '@rollup/plugin-json'
 import hashbang from 'rollup-plugin-hashbang'
 import commonjs from '@rollup/plugin-commonjs'
 import nodeResolve from '@rollup/plugin-node-resolve'
+import { terser } from 'rollup-plugin-terser'
 // const pkg = require('package.json')
 
 // console.log(pkg)
@@ -9,10 +10,9 @@ export default {
   input: 'packages/cli/lib/index.js',
   output: {
     file: 'packages/cli/bin/fantuan.js',
-    format: 'es'
+    format: 'cjs'
   },
-  external: ['electron'],
-  plugins: [hashbang(), json(), nodeResolve(), commonjs()]
+  plugins: [nodeResolve(), hashbang(), json(), commonjs(), terser()]
   //   // 指出应将哪些模块视为外部模块
   //   external: [
   //     ...Object.keys(pkg.dependencies || {}),
