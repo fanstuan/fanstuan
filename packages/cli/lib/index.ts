ae7c26ea640892a24fcbcb85aad9e778f7be045a
L.Rain <bxy2997@sina.com>
2020-06-15 16:45:56 +0800

    feat(typescript): 更好的支持ts

--- packages/cli/lib/index.ts
+++ packages/cli/lib/index.ts
@@ -9,7 +9,7 @@ import { fetchPkg } from 'fetch-pkg'
 // const packageJson = require('package-json')
 
 import fs from 'fs'
-const requiredNodeVersion = require('../package.json')
+import requiredNodeVersion from '../package.json'
 
 // // 检测node版本是否符合要求
 checkNodeVersion(requiredNodeVersion.engines.node, '@fanstuan/cli')
@@ -40,13 +40,13 @@ program
       registryURL: 'https://npm.pkg.github.com',
       token: 'bd7ff84f88c5007229a2f553130cdae3e5a2860b'
     })
-      .then(pkg => {
+      .then((pkg: any) => {
         console.timeEnd()
         pkg.pipe(fs.createWriteStream('template')).once('finish', () => {
           process.exit(0)
         })
       })
-      .catch(e => console.log(e))
+      .catch((e: object) => console.log(e))
   })
 // 处理非法命令
 program.arguments('<command>').action(cmd => {
